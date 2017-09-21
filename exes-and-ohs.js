a5cced869c13dc53956712b103734b4a725953cd
Mercutio <mercutio@MERCUTIO-IMAC-02.local>
2017-09-21 13:26:33 -0700

    exes and ohs

--- exes-and-ohs.js
+++ exes-and-ohs.js
@@ -6,15 +6,39 @@
 // XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
 // XO("zzoo") => false
 
+/*
+This function will take one parameter, str.
+The logic behind this approach is to set a counter to = 0. I want to add to the counter
+everytime my loop reads 'o'. To check to see if a string has the same amout of 'x', I want to
+subtract from the counter everytime my loop reads 'x'. If the string has the same amount of 'x' and 'o'
+then the count SHOULD be 0.
+*/
+
+/*
+Direction:
+
+Step 1: To implement the logic. We need a counter. Call it sum and set it = to 0.
+Step 2: Run a for loop. This will run through the length of the user's input string.
+Step 3: Since the case is insensitive, we can do less work by making everything lowercase.
+		If the loop finds an 'o', make it lowercase and add 1 to the sum counter.
+		Vice versa, if the loop finds an 'x', subtract from the sum counter. 
+Step 4: We now return the sum counter as a boolean. Is it 0? true or false.
+
+*/
+
 
 function XO(str) {
+	//Step 1
   var sum = 0;
+  //Step 2
   for (var i = 0; i < str.length; i++) {
+  	//Step 3
     if (str[i].toLowerCase() == "o") {
       sum++;
     } else if (str[i].toLowerCase() == "x") {
       sum--;
     }
   }
-  return sum == 0
+  //Step 4
+  return sum == 0 ? true:false;
 }
